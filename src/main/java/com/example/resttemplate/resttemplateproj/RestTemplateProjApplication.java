897bc624fc3dc623cb477f78bb06893ca48eda6a
shelly13black <shellymashilo@gmail.com>
2022-04-14 12:42:11 +0200

    New Project for Rest Template for rest services calls.

---
+++ src/main/java/com/example/resttemplate/resttemplateproj/RestTemplateProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.resttemplate.resttemplateproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplateProjApplication.class, args);
+	}
+
+}
